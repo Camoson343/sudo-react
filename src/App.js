e0cc9badeacb69bcc1fab1906fb31407ce4cbdae
Cameron Chafkar <ccdecisions@gmail.com>
2018-06-01 13:49:22 -0700

    work rough draft

--- src/App.js
+++ src/App.js
@@ -7,11 +7,34 @@ import React, { Component } from 'react';
      super(props);
      this.state = {
        todos: [
-         { description: 'Walk the cat', isCompleted: true },
-         { description: 'Throw the dishes away', isCompleted: false },
-         { description: 'Buy new dishes', isCompleted: false }
-       ]
+         { description: 'Walk the cat', isCompleted: true ,},
+         { description: 'Throw the dishes away', isCompleted: false, },
+         { description: 'Buy new dishes', isCompleted: false, }
+       ],
+       newTodoDescription: ''
      };
+   }
+
+   deleteToDo(index){
+     this.setState({ this.state.todos.filter(todo => todo !== index) })
+   }
+
+   handleChange(e){
+     this.setState({ newTodoDescription: e.target.value })
+   }
+
+   handleSubmit(e){
+     e.preventDefault();
+     if (!this.state.newTodoDescription) { return }
+     const newToDo = { description: this.state.newTodoDescription, isCompleted: false };
+     this.setState({ todos: [...this.state.todos, newToDo], newTodoDescription: '' });
+   }
+
+   toggleComplete(index){
+     const todos = this.state.todos.slice();
+     const todo = todos[index];
+     todo.isCompleted = todo.isCompleted ? false : true;
+     this.setState({ todos: todos });
    }
 
    render() {
@@ -19,9 +42,13 @@ import React, { Component } from 'react';
        <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteToDo={ () => this.deleteToDo(index)} />
           )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e)} >
+          <input type="text" value={ this.state.newTodoDescription} onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
        </div>
      );
    }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react';
    render() {
      return (
        <li>
-        <input type="checkbox" checked={ this.props.isCompleted } />
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
         <span>{ this.props.description }</span>
+        <button type="delete" onClick={ this.props.deleteToDo }>delete</button>
        </li>
      );
    }
